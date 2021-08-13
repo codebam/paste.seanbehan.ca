769b7a7b0eb7e9d485218bf887fb0fcccee527b4
Sean Behan <codebam@riseup.net>
2021-08-13 16:38:43 -0400

    add new run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
+export container_name=pastebin-nginx
+
 podman run \
     --detach \
-    --name pastebin-nginx \
+    --name $container_name \
     -p 8082:8080 \
     -v ./website:/var/www/html:z \
-    -v ./nginx.conf:/etc/nginx/conf.d/server.conf:z \
+    -v ./nginx.conf:/etc/nginx/nginx.conf:z \
     trafex/php-nginx
 
-podman exec --user root mkdir /var/www/html/files
-podman exec --user root chmod 777 /var/www/html/files
+podman exec --user root $container_name mkdir /var/www/html/files
+podman exec --user root $container_name chown nobody:nobody /var/www/html/files
