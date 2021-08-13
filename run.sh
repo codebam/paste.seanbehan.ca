8cf4d1e632401e95c7845cd1a69ed0b3e39dc743
Sean Behan <codebam@riseup.net>
2021-08-13 15:56:30 -0400

    update run script

--- run.sh
+++ run.sh
@@ -4,3 +4,6 @@ podman run \
     -p 8082:8080 \
     -v ./website:/var/www/html:z \
     trafex/php-nginx
+
+podman exec --user root mkdir /var/www/html/files
+podman exec --user root chmod 777 /var/www/html/files
