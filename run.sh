59ed6bed94ecba42ad52755db9905d881f1ffc20
Sean Behan <codebam@riseup.net>
2021-08-13 16:28:48 -0400

    add volume mount for custom nginx config

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ podman run \
     --name pastebin-nginx \
     -p 8082:8080 \
     -v ./website:/var/www/html:z \
+    -v ./nginx.conf:/etc/nginx/conf.d/server.conf:z \
     trafex/php-nginx
 
 podman exec --user root mkdir /var/www/html/files
